ed97e6245fd4ba45e62f3b8ce9adc53214bde650
Mili-NT <Mili-NT@gmail.com>
2022-07-15 14:50:22 -0400

    Rewrote isMalicious because it wasn't very good

--- Code-Samples/isMalicious.py
+++ Code-Samples/isMalicious.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
-import hashlib
+from rich import print
 from os import path
+import hashlib
 from random import sample
 from datetime import datetime
 from virus_total_apis import PublicApi as VirusTotalPublicApi
@@ -15,57 +16,67 @@ Purpose: Scans files using the VirusTotal API to check for malware
 
 
 
-api_key = "" # Insert API Key here
-vt = VirusTotalPublicApi(api_key)
+api_key = "" # Insert API Key here, although in practice this should be kept in an environment variable or in an excluded file
 
-def upload_to_vt(file):
-    try:
-        with open(file, "rb") as f:
-            malware_md5 = hashlib.md5(f.read()).hexdigest()
-            print(f"\x1b[94m[MD5 Hash]: \x1b[0m" + f"\x1b[92m{malware_md5}\x1b[0m")
-            response = vt.get_file_report(malware_md5)
-            rsp_code = (response['results'])['response_code']
-            if rsp_code == 0:
-                if path.getsize(file) > 32000000:
-                    print("\x1b[91m Filesize limit of 32mb exceeded\x1b[0m")
-                    exit()
-                else:
-                    file_response = vt.scan_file(file, from_disk=False)
-                    rsp_type = "file"
-                    return file_response, rsp_type
-            else:
-                rsp_type = "lookup"
-                return response, rsp_type
-    except Exception as e:
-        print(f"{e}")
-        exit()
-def get_names(scans_dict):
-    names = []
-    for scans in scans_dict.keys():
-        vals = scans_dict[scans]
-        if vals['detected'] is True:
-            names.append(vals['result'])
-    return sample(names, 5)
-def display(rsp_dict, rsp_type):
-    results = rsp_dict['results']
-    if rsp_type == 'file':
-        print("\x1b[94m[----- Results -----]\x1b[0m")
-        print(f"\x1b[94mResponse: \x1b[0m" + f"\x1b[92m{results['verbose_msg']}\x1b[0m")
-        print(f"\x1b[94mLink to scan: \x1b[0m" + f"\x1b[92m{results['permalink']}\x1b[0m")
-        print("\x1b[94m[-------------------]\x1b[0m")
-    else:
-        scans = results['scans']
-        names = get_names(scans)
-        print("\x1b[94m[----- Results -----]\x1b[0m")
-        print(f"\x1b[94m[Total Results]: \x1b[0m" + f"\x1b[92m{results['total']}\x1b[0m")
-        print(f"\x1b[94m[Percentage Flagged]: \x1b[0m" + f"\x1b[92m{100 * float(results['positives']) // float(results['total'])}%\x1b[0m")
-        print(f"\x1b[94m[Known as]: \x1b[0m\x1b[92m", end = '', flush=True)
-        print(*names, sep=', ', end='\x1b[0m\n')
-        print(f"\x1b[94m[Link To Scan]: \x1b[0m" + f"\x1b[92m{results['permalink']}\x1b[0m")
-        print("\x1b[94m[-------------------]\x1b[0m")
-def main(filename):
-    rsp, rsp_type = upload_to_vt(filename)
-    display(rsp, rsp_type)
+class Scan:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.api = VirusTotalPublicApi(api_key)
+
+    def get_file_hash(self):
+        with open(self.filepath, "rb") as f:
+            return hashlib.md5(f.read()).hexdigest()
+
+    def check_file_size(self):
+        if path.getsize(self.filepath) > 32000000:
+            print(self.format("error", "VT filesize limit of 32mb exceeded."))
+            exit()
+
+    def format(self, str_type, *str):
+        if str_type == "item":
+            return f"[bold blue]{str[0]}[/bold blue]: [green]{', '.join(str[1:])}[/green]\n"
+        elif str_type == "item_percent":
+            item_color = "bold red" if str[1] > 0 else "green"
+            return f"[bold blue]{str[0]}[/bold blue]: [{item_color}]{str[1]}%[/{item_color}]\n"
+        elif str_type == "line":
+            return f"[bold blue]{str[0]}[/bold blue]]\n"
+        elif str_type == "error":
+            return f"[bold red]{str[0]}[/bold red]\n"
+        else:
+            return str
+
+    def display(self, results):
+        isQueued = True if 'queued' in results['verbose_msg'] else False
+        display_str = self.format('line', """"[----- Results -----]""")
+        display_str += self.format("item", "Link to Scan", results['permalink'])
+        if isQueued:
+            display_str += self.format("line", results['verbose_msg'])
+        else:
+            display_str += self.format("item", "[Total Results]", f"{results['total']}")
+            display_str += self.format("item_percent", "Percentage Flagged", 100 * float(results['positives']) // float(results['total']))
+            display_str += self.format("item", "[Known as]", *sample(results['names'], 10))
+        display_str += self.format('line', "[-------------------]")
+        print(display_str)
+
+    def get_scan_info(self):
+        response = self.api.get_file_report(self.get_file_hash())
+        if response['response_code'] != 200:
+            print(f"[bold red]Non-200 Response Code: {response['response_code']}. Check connection and site status.[/bold red]")
+            exit()
+        if response['results']['response_code'] != 0:
+            results = response['results']
+            scan_list = results['scans']
+            results['names'] = [scan_list[scan]['result'] for scan in scan_list if scan_list[scan]['result']]
+        else:
+            results = self.api.scan_file(self.filepath, from_disk=False)['results']
+        return results
+
+    def scan(self):
+        scan_results = self.get_scan_info()
+        self.display(scan_results)
+
+def main(filepath):
+    Scan(filepath).scan()
 
 
 if __name__ == "__main__":
@@ -75,4 +86,4 @@ if __name__ == "__main__":
         finish = datetime.now()
         print(f"Runtime: {finish - start}")
     except IndexError:
-        print(f"Usage: {sys.argv[0]} <filename>")
+        print(f"Usage: {sys.argv[0]} <filepath>")
